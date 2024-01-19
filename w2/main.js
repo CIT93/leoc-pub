199f72b967b75ac9c1534001d0b4e4b9c031e303
leocontreras559 <leonardlcontreras559@gmail.com>
2024-01-18 19:23:24 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,11 @@
+const myvalue1 = 10 
+const myvalue2 = 2
+const myvalue3 = 10
+const myvalue4 = 2
+const myvalue5 = 6
+const myvalue6 = 40
+const myvalue7 = 20
+const myvalue8 = 10
+
+const total = myvalue1 + myvalue2 + myvalue3 + myvalue4 + myvalue5 + myvalue6 + myvalue7 + myvalue8
+
