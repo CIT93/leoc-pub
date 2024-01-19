039e77a43cfb9526dc768926e3f0adad6a7eb24f
leocontreras559 <leonardlcontreras559@gmail.com>
2024-01-18 20:14:28 -0800

    coding done fixed

--- w2/main.js
+++ w2/main.js
@@ -1,11 +1,11 @@
-const myvalue1 = 10
+const myvalue1 = 7
 const myvalue2 = 2
 const myvalue3 = 10
 const myvalue4 = 2
 const myvalue5 = 6
-const myvalue6 = 40
+const myvalue6 = 5
 const myvalue7 = 20
-const myvalue8 = 10
+const myvalue8 = 5
 
 const total = myvalue1 + myvalue2 + myvalue3 + myvalue4 + myvalue5 + myvalue6 + myvalue7 + myvalue8
-
+const carbontotal = document.querySelector("h2");
