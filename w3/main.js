15dd1f1f398f33db0397db9f29818a775b4ee289
leocontreras559 <leonardlcontreras559@gmail.com>
2024-01-25 20:20:27 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -18,10 +18,30 @@ if (numberinHousehold === 1){
 
 console.log("based on the number of members in the household of $(numberinHousehold) the points would be $(carbonfootprintPoints).");
 
-let carbonfootprintPts = 0;
-const numberinHousehold = 3;
+//let carbonfootprintPts = 0;
+//const numberinHousehold = 3;
 
 // global scope
 
 determinehouseholdPts(3)
 determinehouseholdPts(4)
+
+
+// size of house carbon footprint
+
+
+let carbonfootprintPts = 0;
+const sizeofhouse = "large";
+
+if (sizeofhouse === "large"){
+    carbonfootprintPts = carbonfootprintPts + 10;
+} else if(sizeofhouse === "medium"){
+    carbonfootprintPts = carbonfootprintPts + 7;
+} else if(sizeofhouse === "small"){
+    carbonfootprintPts = carbonfootprintPts + 4;
+} else if(sizeofhouse === "apartment"){
+    carbonfootprintPts = carbonfootprintPts + 2;
+}
+
+console.log("Based on the size of $(sizeofhouse) the points would be $(carbonfootprintsPts)")
+
