94b66abf1e28c718f0e9624d3b9f08b23d8f6203
leocontreras559 <leonardlcontreras559@gmail.com>
2024-02-02 14:35:18 -0800

    updated output to DOM

--- w4/main.js
+++ w4/main.js
@@ -1,60 +1,66 @@
-const cfpData = [];
+const cfpData = [1];
 
-function determineHouseSizePts(size){
-     console.log("inside block scope")
-     let houseSizePts = 0;
-    if(size === "large"){
-        houseSizePts = 10;
-    } else if (size === "medium"){
-        houseSizePts = 7;
-    } else if (size === "small"){
-        houseSizePts = 4;
-    } else if (size === "apt"){
-        houseSizePts = 2;
-    }
+function determineHouseSizePts(size) {
+  console.log("inside block scope");  let houseSizePts = 0;
+  if (size === "large") {
+    houseSizePts = 10;
+  } else if (size === "medium") {    houseSizePts = 7;
+  } else if (size === "small") {
+    houseSizePts = 4;
+  } else if (size === "apt") {
+    houseSizePts = 2;  }
 
-return houseSizePts; 
+  return houseSizePts;
 }
 
-function determineHouseHoldPts(numbersInHousehold){
-    console.log("inside block scope");
-    let houseHoldPoints = 0;
-if (numbersInHousehold === 1 ) {
-houseHoldPoints = 14;
-} else if(numbersInHousehold === 2){
+function determineHouseHoldPts(numbersInHousehold) {
+  console.log("inside block scope");
+  let houseHoldPoints = 0;
+  if (numbersInHousehold === 1) {
+    houseHoldPoints = 14;
+  } else if (numbersInHousehold === 2) {
     houseHoldPoints = 12;
-} else if (numbersInHousehold === 3){
-    houseHoldPoints = 10; 
-} else if (numbersInHousehold === 4){
-    houseHoldPoints = 8; 
-} else if (numbersInHousehold === 5){
-    houseHoldPoints = 6; 
-}else if (numbersInHousehold === 6){
-    houseHoldPoints = 4; 
-} 
- else if (numbersInHousehold > 6){
-    houseHoldPoints = 2; 
+  } else if (numbersInHousehold === 3) {
+    houseHoldPoints = 10;
+  } else if (numbersInHousehold === 4) {
+    houseHoldPoints = 8;
+  } else if (numbersInHousehold === 5) {
+    houseHoldPoints = 6;
+  } else if (numbersInHousehold === 6) {
+    houseHoldPoints = 4;
+  } else if (numbersInHousehold > 6) {
+    houseHoldPoints = 2;
+  }
+
+  return houseHoldPoints;
 }
 
-    return houseHoldPoints; 
-}
-
-function start(houseHoldMembers, houseSize){
-   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
-   const houseSizePTS = determineHouseSizePts(houseSize);
-   const total = houseHoldPTS + houseSizePTS;
-   cfpData.push([houseHoldMembers, houseSize, houseHoldPTS, houseSizePTS, total]);
-   
-  
+function start(houseHoldMembers, houseSize) {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePTS = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePTS;
+  cfpData.push([
+    houseHoldMembers,
+    houseSize,
+    houseHoldPTS,
+    houseSizePTS,
+    total,
+  ]);
 }
 
 function displayOutput() {
-    
-}
+  for (arr of cfpData) {
+    console.log(arr);
+    const output = document.getElementById("output");
+    const newP = document.createElement("p");
+    newP.textContent = `Carbon Footprint total is ${arr[4]}`;
+   output.appendChild(newP)
+  }
+} 
 
-start(5, "apt");
-start(4, "large");
-start(3, "medium");
-start(2, "small");
+start(5, "large");
+start(4, "medium");
+start(3, "small");
+start(2, "apt");
 
 displayOutput()
